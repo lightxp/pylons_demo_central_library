5a0a8c97bbebaf3b709f390691e408d1825f82fe
Jon Rosebaugh <jon@inklesspen.com>
2009-01-28 09:31:47 -0800

    Websetup.py / create model and initial data.

--- central_library/websetup.py
+++ central_library/websetup.py
@@ -1,6 +1,8 @@
 """Setup the central_library application"""
 import logging
 
+import datetime
+
 from central_library.config.environment import load_environment
 
 log = logging.getLogger(__name__)
@@ -15,3 +17,16 @@ def setup_app(command, conf, vars):
 
     # Create the tables if they aren't there already
     meta.metadata.create_all(checkfirst=True)
+    
+    from central_library.model import User, Copy, Item
+
+    quentin = User(u"Quentin McUserpants", u"123 E. Main St.\nBoondocksville, XY, 12345")
+    
+    book1 = Item(u"A Million Random Digits with 100,000 Normal Deviates", u"RAND Corporation", 9780833030474, datetime.date(2002, 12, 25), "book")
+    book2 = Item(u"Watchmen", u"Alan Moore and Dave Gibbons", 9780930289232, datetime.date(1995, 4, 1), "book")
+    copy1 = Copy(book1, datetime.date(2005, 1, 1), u"dogeared")
+    copy2 = Copy(book1, datetime.date.today(), u"brand new")
+    copy3 = Copy(book2, datetime.date.today(), u"brand new")
+    sess = meta.Session()
+    sess.add_all([quentin, book1, book2, copy1, copy2, copy3])
+    sess.commit()
